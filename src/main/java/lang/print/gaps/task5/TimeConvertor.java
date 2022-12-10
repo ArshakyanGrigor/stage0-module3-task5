9c329832803daa033f96714ff1070efea94c6580
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-10 15:51:38 +0400

    Updated file

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        int second = 60;
+        double minutesToSecond = minutes * second;
+        System.out.println(minutesToSecond);
     }
 }
